46e96eb66ec2d177690fa976d2705b194f6e90ad
MDJAmin <166019288+MDJAmin@users.noreply.github.com>
2025-03-16 15:43:33 +0330

    Feat: Skills

--- src/Components/sections/About.jsx
+++ src/Components/sections/About.jsx
@@ -1,44 +1,42 @@
 import { RevealOnScroll } from "../Common/RevealOnScroll";
 
 export const About = () => {
-  const frontendSkills = [
-    "React",
-    "Vue",
-    "TypeScript",
-    "TailwindCSS",
-    "Svelte",
-  ];
+  const frontendSkills = ["React", "TypeScript", "TailwindCSS", "MUI", "ThreeJs", "Gsap"];
 
   const backendSkills = ["Node.js", "Python", "AWS", "MongoDB", "GraphQL"];
 
+  const designSkills = ["Adobe Pro", "Figma", "Blender", "AE", "AI"];
+
+  const otherSkill = ["Model Optimization", "DQN", "Computer Vision", "NLP"];
+
   return (
     <section
-      id="about"
-      className="min-h-screen flex items-center justify-center py-20"
+      id='about'
+      className='min-h-screen flex items-center justify-center py-20'
     >
       <RevealOnScroll>
-        <div className="max-w-3xl mx-auto px-4">
-          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-white to-gray-400 bg-clip-text text-transparent text-center">
+        <div className='max-w-3xl mx-auto px-4'>
+          <h2 className='text-3xl font-bold mb-8 bg-gradient-to-r from-white to-gray-400 bg-clip-text text-transparent text-center'>
             {" "}
             About Me
           </h2>
 
-          <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
-            <p className="text-gray-300 mb-6">
-              Passionate developer with expertise in building scalable web
-              applications and creating innovative solutions.
+          <div className='rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all'>
+            <p className='text-gray-300 mb-6'>
+              Passionate developer with expertise in building scalable web applications and
+              creating innovative solutions.
             </p>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                <h3 className="text-xl font-bold mb-4"> Frontend</h3>
-                <div className="flex flex-wrap gap-2">
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+              <div className='rounded-xl p-6 hover:-translate-y-1 transition-all'>
+                <h3 className='text-xl font-bold mb-4'> Frontend</h3>
+                <div className='flex flex-wrap gap-2'>
                   {frontendSkills.map((tech, key) => (
                     <span
                       key={key}
-                      className="bg-blue-500/10 text-white py-1 px-3 rounded-full text-sm hover:bg-gray-700 cursor-pointer 
+                      className='bg-blue-500/10 text-white py-1 px-3 rounded-full text-sm hover:bg-gray-700 cursor-pointer 
                                     hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
-                    "
+                    '
                     >
                       {tech}
                     </span>
@@ -46,15 +44,47 @@ export const About = () => {
                 </div>
               </div>
 
-              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                <h3 className="text-xl font-bold mb-4"> Backend</h3>
-                <div className="flex flex-wrap gap-2">
+              <div className='rounded-xl p-6 hover:-translate-y-1 transition-all'>
+                <h3 className='text-xl font-bold mb-4'> Backend</h3>
+                <div className='flex flex-wrap gap-2'>
                   {backendSkills.map((tech, key) => (
                     <span
                       key={key}
-                      className="bg-blue-500/10 text-white py-1 px-3 rounded-full text-sm hover:bg-gray-700 cursor-pointer 
+                      className='bg-blue-500/10 text-white py-1 px-3 rounded-full text-sm hover:bg-gray-700 cursor-pointer 
                                     hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
-                    "
+                    '
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+              <div className='rounded-xl p-6 hover:-translate-y-1 transition-all'>
+                <h3 className='text-xl font-bold mb-4'> Design</h3>
+                <div className='flex flex-wrap gap-2'>
+                  {designSkills.map((tech, key) => (
+                    <span
+                      key={key}
+                      className='bg-blue-500/10 text-white py-1 px-3 rounded-full text-sm hover:bg-gray-700 cursor-pointer 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+                    '
+                    >
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+
+              <div className='rounded-xl p-6 hover:-translate-y-1 transition-all'>
+                <h3 className='text-xl font-bold mb-4'> Other</h3>
+                <div className='flex flex-wrap gap-2'>
+                  {otherSkill.map((tech, key) => (
+                    <span
+                      key={key}
+                      className='bg-blue-500/10 text-white py-1 px-3 rounded-full text-sm hover:bg-gray-700 cursor-pointer 
+                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
+                    '
                     >
                       {tech}
                     </span>
@@ -64,43 +94,32 @@ export const About = () => {
             </div>
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
-            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
-              <ul className="list-disc list-inside text-gray-300 space-y-2">
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-8'>
+            <div className='p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all'>
+              <h3 className='text-xl font-bold mb-4'> 🏫 Education </h3>
+              <ul className='list-disc list-inside text-gray-300 space-y-2'>
                 <li>
-                  <strong> B.S. in Computer Science </strong> - XYZ University
-                  (2016-2020)
+                  <strong> B.S. in Computer Science </strong> - XYZ University (2016-2020)
                 </li>
                 <li>
-                  Relevant Coursework: Data Structures, Web Development, Cloud
-                  Computing...
+                  Relevant Coursework: Data Structures, Web Development, Cloud Computing...
                 </li>
               </ul>
             </div>
-            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
-              <h3 className="text-xl font-bold mb-4"> 💼 Work Experience </h3>
-              <div className="space-y-4 text-gray-300">
+            <div className='p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all'>
+              <h3 className='text-xl font-bold mb-4'> 💼 Work Experience </h3>
+              <div className='space-y-4 text-gray-300'>
                 <div>
-                  <h4 className="font-semibold">
+                  <h4 className='font-semibold'>
                     {" "}
                     Software Engineer at ABC Corp (2020 - Present){" "}
                   </h4>
-                  <p>
-                    Developed and maintained microservices for cloud-based
-                    applications.
-                  </p>
+                  <p>Developed and maintained microservices for cloud-based applications.</p>
                 </div>
 
                 <div>
-                  <h4 className="font-semibold">
-                    {" "}
-                    Intern at DEF Startups (2019){" "}
-                  </h4>
-                  <p>
-                    Assisted in building front-end components and integration
-                    REST APIs
-                  </p>
+                  <h4 className='font-semibold'> Intern at DEF Startups (2019) </h4>
+                  <p>Assisted in building front-end components and integration REST APIs</p>
                 </div>
               </div>
             </div>
